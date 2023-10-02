a533c01b7a72723dd0ab2747ab3c77ac416bc544
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-10-02 19:21:04 -0400

    Allow ECR to get

--- ecr.tf
+++ ecr.tf
@@ -7,3 +7,39 @@ resource "aws_ecr_repository" "module" {
     scan_on_push = false
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "module" {
+  statement {
+    sid    = "ECR"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "module" {
+  repository = aws_ecr_repository.module.name
+  policy     = data.aws_iam_policy_document.module.json
+}
