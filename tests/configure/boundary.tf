437683964817c68f0516419654ed9113e7b2e502
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-10-04 12:00:50 -0400

    Configure credentials store for module testing

--- tests/configure/boundary.tf
+++ tests/configure/boundary.tf
@@ -14,3 +14,23 @@ resource "boundary_scope" "test" {
   auto_create_admin_role   = true
   auto_create_default_role = true
 }
+
+resource "vault_token" "boundary_controller" {
+  policies          = []
+  no_default_policy = false
+  no_parent         = true
+  ttl               = "3d"
+  explicit_max_ttl  = "6d"
+  period            = "3d"
+  renewable         = true
+  num_uses          = 0
+}
+
+resource "boundary_credential_store_vault" "test" {
+  name        = "vault"
+  description = "Vault credentials store"
+  address     = data.terraform_remote_state.infrastructure.outputs.hcp_vault_public_address
+  token       = vault_token.boundary_controller.client_token
+  namespace   = data.terraform_remote_state.infrastructure.outputs.hcp_vault_namespace
+  scope_id    = boundary_scope.test.id
+}
--- tests/configure/outputs.tf
+++ tests/configure/outputs.tf
@@ -1,3 +1,11 @@
-output "products_infra_scope_id" {
-  value = boundary_scope.test.id
+output "boundary_scope_ids" {
+  value = {
+    "modules" = boundary_scope.test.id
+  }
+}
+
+output "boundary_credentials_store_ids" {
+  value = {
+    "modules" = boundary_credential_store_vault.test.id
+  }
 }
