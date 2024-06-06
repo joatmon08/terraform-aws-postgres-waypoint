3a66a0cf193aa736f2c0157575128ce9e69eb6e4
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-06-06 13:37:51 +0200

    Remove encryption

--- vault-database-secrets.tf
+++ vault-database-secrets.tf
@@ -21,7 +21,7 @@ resource "vault_database_secret_backend_connection" "db" {
   allowed_roles = ["*"]
 
   postgresql {
-    connection_url = "postgresql://{{username}}:{{password}}@${aws_db_instance.database.address}:${var.postgres_port}/${var.db_name}?sslmode=disable"
+    connection_url = "postgresql://{{username}}:{{password}}@${aws_db_instance.database.address}:${var.postgres_port}/${var.db_name}"
     username       = aws_db_instance.database.username
     password       = aws_db_instance.database.password
   }
@@ -31,7 +31,7 @@ resource "vault_database_secret_backend_role" "db" {
   backend               = vault_mount.db.path
   name                  = var.db_name
   db_name               = vault_database_secret_backend_connection.db.name
-  creation_statements   = ["CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"]
+  creation_statements   = ["CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ${var.db_name} TO \"{{name}}\"; GRANT INSERT ON ${var.db_name} TO \"{{name}}\";"]
   revocation_statements = ["ALTER ROLE \"{{name}}\" NOLOGIN;"]
   default_ttl           = 3600
   max_ttl               = 3600
